787333ff0b1fcb80b6989a23dbe0c003e6df2c11
Сороковов Андрей Алексеевич <sorokovovaa@vfudgu.local>
2025-03-24 12:48:09 +0300

    Добавьте файлы проекта.

---
+++ HarryPotter.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm> // эта библиотека нужна для функции transform
+#include <fstream>
+
+using namespace std;
+
+vector<vector<string>> SentencesParser(string text)
+{
+	vector<vector<string>> result;
+
+
+
+	return result;
+}
+
+bool IsLetter(string l)
+{
+	if ((l >= "A" && l <= "Z") || (l >= "a" && l <= "z")
+		|| (l >= "А" && l <= "Я")
+		|| (l >= "а" && l <= "я")
+		|| (l == "ё")
+		|| (l == "Ё")
+		|| (l == "\'"))
+		return true;
+	return false;
+}
+
+
+int main()
+{
+	string source;
+	transform(source.begin(), source.end(), source.begin(), tolower);	// перевод строки в нижний регистр
+}
